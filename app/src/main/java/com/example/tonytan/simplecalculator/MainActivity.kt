d2b89c79ddecceed07c94752ca0d31e3334e3b1f
widmonstertony <t347100955@gmail.com>
2018-10-17 01:57:25 -0700

    Finished

--- app/src/main/java/com/example/tonytan/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/tonytan/simplecalculator/MainActivity.kt
@@ -4,14 +4,34 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
+import android.widget.Toolbar
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
-class MainActivity : AppCompatActivity() {
+import android.widget.TextView
+import android.widget.EditText
 
+
+class MainActivity : AppCompatActivity() {
+    private val operand1: EditText? = null
+    private val operand2: EditText? = null
+    private val btnAddition: Button? = null
+    private val btnSubtraction: Button? = null
+    private val btnDivision: Button? = null
+    private val btnMultiplication: Button? = null
+    private val txtResult: TextView? = null
+    private val btnClear: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        operand1 = (EditText) findViewById(R.id.input1);
+        operand2 = (EditText) findViewById(R.id.input2);
+        btnAddition = (Button) findViewById(R.id.plus);
+        btnSubtraction = (Button) findViewById(R.id.minus);
+        btnDivision = (Button) findViewById(R.id.divide);
+        btnMultiplication = (Button) findViewById(R.id.multiply);
+        txtResult = (TextView) findViewById(R.id.result);
+        btnClear = (Button) findViewById(R.id.clear);
         setSupportActionBar(toolbar)
 
     }
